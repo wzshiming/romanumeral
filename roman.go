65ff9e1101c76298f1950a61a71d70a58bedd66e
wzshiming <wzshiming@foxmail.com>
2019-08-12 16:32:12 +0800

    fix

--- roman.go
+++ roman.go
@@ -18,7 +18,7 @@ var romanLetterValue = map[rune]Roman{
 var romanValues = []Roman{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
 var romanLetters = []string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
 
-type Roman uint
+type Roman uint16
 
 func (r Roman) IsValid() bool {
 	return r > 0 && r < 4000
